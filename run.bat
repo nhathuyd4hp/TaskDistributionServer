0250f368dda4c1c0853268391c73a8df96e6bdac
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-09 15:05:36 +0700

    Run Server

---
+++ run.bat
@@ -0,0 +1,30 @@
+@echo off
+set N=%1
+if "%N%"=="" set N=5
+
+echo ===== GIT PULL =====
+git pull origin main
+if errorlevel 1 (
+    pause
+    exit /b 1
+)
+
+echo ===== SYNC ENV =====
+uv sync
+if errorlevel 1 (
+    pause
+    exit /b 1
+)
+
+echo ===== START SERVER =====
+start "api-8000" ./.venv/Scripts/python -m uvicorn src.main:app --host 0.0.0.0 --port 8000
+
+echo ===== START WORKER =====
+for /L %%i in (1,1,%N%) do (
+    start "celery-%%i" ./.venv/Scripts/python -m celery -A src.worker.Worker worker ^
+        --hostname=worker%%i@%COMPUTERNAME% ^
+        --pool=solo ^
+        --concurrency=1 ^
+        --prefetch-multiplier=1 ^
+        --max-tasks-per-child=1
+)
