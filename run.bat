5e7ffe4e33abe5f923be1dabefc5338af399bac6
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-12 15:34:48 +0700

    fix(run.bat): update default worker count and improve process naming

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 set N=%1
-if "%N%"=="" set N=5
+if "%N%"=="" set N=8
 
 echo ===== GIT PULL =====
 git pull origin main
@@ -17,11 +17,11 @@ if errorlevel 1 (
 )
 
 echo ===== START SERVER =====
-start "api-8000" ./.venv/Scripts/python -m uvicorn src.main:app --host 0.0.0.0 --port 8000
+start "API Server" ./.venv/Scripts/python -m uvicorn src.main:app --host 0.0.0.0 --port 8000
 
 echo ===== START WORKER =====
 for /L %%i in (1,1,%N%) do (
-    start "celery-%%i" ./.venv/Scripts/python -m celery -A src.worker.Worker worker ^
+    start "Worker-%%i" ./.venv/Scripts/python -m celery -A src.worker.Worker worker ^
         --hostname=worker%%i@%COMPUTERNAME% ^
         --pool=solo ^
         --concurrency=1 ^
