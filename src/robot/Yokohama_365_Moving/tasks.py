b630054f6f6d23cef90f83132e822207ad780328
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-13 09:49:05 +0700

    refactor(tasks): reorganize shared_task decorator for Yokohama function

--- src/robot/Yokohama_365_Moving/tasks.py
+++ src/robot/Yokohama_365_Moving/tasks.py
@@ -3,8 +3,14 @@ from pathlib import Path
 
 from celery import shared_task
 
+from src.core.inactive_task import InactiveTask
 
-@shared_task(bind=True, name="Yokohama 365 Moving")
+
+@shared_task(
+    bind=True,
+    name="Yokohama 365 Moving",
+    base=InactiveTask,
+)
 def Yokohama(self):
     exe_path = (
         Path(__file__).resolve().parents[2] / "robot" / "Yokohama_365_Moving" / "sharepoint_folder_moving_V1_1.exe"
