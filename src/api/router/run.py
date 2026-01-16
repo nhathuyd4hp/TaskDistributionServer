7e52969157ca5012b1f98cc6aa0feedbaa9db6f5
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-16 14:33:48 +0700

    fix(Run): lấy kết quả ở asset API

--- src/api/router/run.py
+++ src/api/router/run.py
@@ -29,23 +29,16 @@ def get_histories(
         histories = RunService(session).findMany()
     return SuccessResponse(data=histories)
 
-
-@router.get(path="/result/{id}", name="Lấy kết quả chạy")
-def get_result(id: str, session: Session = Depends(get_session)):
+@router.get(
+    path="/{id}",
+    name="Lịch sử chạy",
+    response_model=SuccessResponse,
+)
+def get_histories(
+    id: str,
+    session: Session = Depends(get_session),
+):
     history = RunService(session).findByID(id)
-    if not history:
+    if history is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="run not found")
-    if history.status != Status.SUCCESS:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="run not successful")
-    response = ResultService.get_object(
-        bucket_name=settings.RESULT_BUCKET,
-        object_name=history.result,
-    )
-    data = BytesIO(response.read())
-    response.close()
-    response.release_conn()
-    return StreamingResponse(
-        data,
-        media_type="application/octet-stream",
-        headers={"Content-Disposition": f'attachment; filename="{os.path.basename(history.result)}"'},
-    )
+    return SuccessResponse(data=history)
