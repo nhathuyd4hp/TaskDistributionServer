e4585f7cdd138b8fa4985d645dd746c37ae8ab7c
Văn Ngọc Nhật Huy <nhathuyd4hp@gmail.com>
2026-01-27 11:23:42 +0700

    Update API Server
    Disable broadcast
    Disable Redocs

--- src/api/dependency.py
+++ src/api/dependency.py
@@ -1,12 +1,7 @@
-import secrets
-
-from fastapi import Depends, HTTPException, Request, status
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi import Request
 from sqlmodel import Session
 
 from src.core.config import settings
-
-security = HTTPBasic()
 
 
 def get_session():
@@ -16,20 +11,3 @@ def get_session():
 
 def get_scheduler(request: Request):
     return request.app.state.scheduler
-
-
-def required_admin(credentials: HTTPBasicCredentials = Depends(security)):
-    is_correct_username = secrets.compare_digest(
-        credentials.username.encode("utf8"), settings.ADMIN_USERNAME.encode("utf8")
-    )
-    is_correct_password = secrets.compare_digest(
-        credentials.password.encode("utf8"), settings.ADMIN_PASSWORD.encode("utf8")
-    )
-
-    if not (is_correct_username and is_correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
